52e651ccd56c02fe4e576d8bfde23306c7299c14
MkaGodh <fenryrvla@yandex.ru>
2024-06-20 16:40:02 +0300

    Добавьте файлы проекта.

---
+++ HorizontalCut/HorizontalCut.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int minecraft[5][5][10];
+
+    cout << "Please, input matrix of heights: " << "\n";
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cin >> n;
+            for (int k = 0; k < 10; k++) {
+                minecraft[i][j][k] = n;
+            }
+        }
+    }
+    int slice;
+    cout << "Please input slice: " << "\n";
+    cin >> slice;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (minecraft[i][j][slice] >= slice) {
+                cout << "1" << " ";
+            }
+            else {
+                cout << "0" << " ";
+            }
+        }
+        cout << "\n";
+    }
+
+}
